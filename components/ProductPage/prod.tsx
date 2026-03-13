727ca5ac811e85c882b88b4f4d93330ffbc802ee
SharjeelKazim <sharjeelqazim81@gmail.com>
2026-03-13 05:23:32 +0500

    add Products

--- components/ProductPage/prod.tsx
+++ components/ProductPage/prod.tsx
@@ -1,9 +1,86 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+"use client";
 
-const prod = () => {
+import React from "react";
+import { BiShoppingBag } from "react-icons/bi";
+
+const products = [
+  {
+    id: 1,
+    name: "SPIRIT ROSE",
+    price: "$1500",
+    image: "/watch.png",
+  },
+  {
+    id: 2,
+    name: "KHAKI PILOT",
+    price: "$1350",
+    image: "/watch.png",
+  },
+  {
+    id: 3,
+    name: "JUBILEE BLACK",
+    price: "$870",
+    image: "/watch.png",
+  },
+  {
+    id: 4,
+    name: "FOSIL ME3",
+    price: "$650",
+    image: "/watch.png",
+  },
+  {
+    id: 5,
+    name: "DUCHEN",
+    price: "$950",
+    image: "/watch.png",
+  },
+];
+
+const Prod = () => {
   return (
-    <div>prod</div>
-  )
-}
+    <section className="w-full bg-gray-100 py-20">
+      <div className="w-[90%] mx-auto">
 
-export default prod
+        {/* Heading */}
+        <div className="text-center mb-16">
+          <div className="w-12 h-0.5 bg-[#FFB568] mx-auto mb-4"></div>
+          <h2 className="tracking-widest text-gray-700">PRODUCTS</h2>
+        </div>
+
+        {/* Grid */}
+        <div className="grid grid-cols-4 gap-10 justify-items-center">
+
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className="relative bg-white w-62.5 p-6 text-center shadow-md hover:shadow-xl transition"
+            >
+              <img
+                src={product.image}
+                alt={product.name}
+                className="w-35 mx-auto mb-6 hover:scale-110 transition-transform duration-500"
+              />
+
+              <h3 className="text-sm font-semibold mb-2">
+                {product.name}
+              </h3>
+
+              <p className="text-[#FFB568] font-semibold">
+                {product.price}
+              </p>
+
+              {/* Cart Icon Box */}
+              <div className="absolute bottom-0 right-0 bg-black p-3 cursor-pointer hover:bg-gray-800 transition">
+                <BiShoppingBag className="text-white text-lg" />
+              </div>
+            </div>
+          ))}
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Prod;
